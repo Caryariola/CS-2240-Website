da54811a1cab51f2963e540f7a3f4342687755a2
Cary Ariola <carygomezariola@gmail.com>
2026-01-31 21:05:15 +0800

    change bg-color in contact and about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="flex flex-col p-10 border m-3.5 max-w-screen gap-4 items-center min-h-screen bg-zinc-50 font-sans dark:bg-black ">
+    <div className="flex flex-col p-10 border m-3.5 max-w-screen gap-4 items-center min-h-screen  font-sans">
       <div className=" border p-2 text-2xl">
        Welcome to my About Page!
       </div>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black text-4xl">
+        <div className="flex min-h-screen items-center justify-center font-sans text-4xl">
             Welcome to my Contact Page!
         </div>
     )
